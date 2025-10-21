032f4d8edcced4a4ad19c5ae2e93c96d44d7b06d
soumitradev <soumitra.shewale.dev@gmail.com>
2025-10-21 12:08:23 +0000

    feat: add login/redirect functionality for pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,31 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { Button } from "@/components/ui/button";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { client } from "./api/$";
 
 export const Route = createFileRoute("/")({
+	loader: async () => {
+		const res = await client.api.user.$get();
+		console.log(res.status);
+		if (res.status !== 200) {
+			throw redirect({
+				to: "/login",
+			});
+		}
+		const user = await res.json();
+		if ("error" in user) {
+			throw redirect({
+				to: "/login",
+			});
+		}
+		return user;
+	},
 	component: App,
 });
 
 function App() {
+	const user = Route.useLoaderData();
 	return (
-		<div className="h-screen flex flex-col items-center justify-center">
-			<Button>Help Yourself</Button>
+		<div className="p-2">
+			<h3>{JSON.stringify(user)}</h3>
 		</div>
 	);
 }
---
+++ src/routes/login.tsx
@@ -0,0 +1,77 @@
+import { createFileRoute, redirect, useNavigate } from "@tanstack/react-router";
+import { useId } from "react";
+import { Button } from "@/components/ui/button";
+import { Field, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { client } from "./api/$";
+
+export const Route = createFileRoute("/login")({
+	loader: async () => {
+		const res = await client.api.user.$get();
+		if (res.status === 200) {
+			throw redirect({
+				to: "/",
+			});
+		}
+	},
+	component: Login,
+});
+
+function Login() {
+	const usernameId = useId();
+	const passwordId = useId();
+	const navigate = useNavigate();
+
+	const handleLogin = async (formData: FormData) => {
+		const username = formData.get("username");
+		const password = formData.get("password");
+		if (username === null || password === null) return;
+		const res = await (
+			await client.api.login.$post({
+				json: {
+					username: username as string,
+					password: password as string,
+				},
+			})
+		).json();
+
+		if ("error" in res) {
+			alert(res.error);
+		}
+		navigate({
+			to: "/",
+		});
+	};
+
+	return (
+		<div className="h-screen w-full flex justify-center">
+			<form className="w-2/3 pt-48" action={handleLogin}>
+				<FieldSet>
+					<FieldGroup>
+						<Field>
+							<FieldLabel htmlFor={usernameId}>Username</FieldLabel>
+							<Input
+								id={usernameId}
+								type="text"
+								placeholder="Username"
+								name="username"
+							/>
+						</Field>
+						<Field>
+							<FieldLabel htmlFor={passwordId}>Password</FieldLabel>
+							<Input
+								id={passwordId}
+								type="password"
+								placeholder="Password"
+								name="password"
+							/>
+						</Field>
+						<Field>
+							<Button type="submit">Login</Button>
+						</Field>
+					</FieldGroup>
+				</FieldSet>
+			</form>
+		</div>
+	);
+}
