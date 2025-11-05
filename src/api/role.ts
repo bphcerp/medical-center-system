782b7d5cbbf4eee96042fd9190e9e6f75994a66e
Mohammed Zeeshan Valappil <66509181+zeeshanok@users.noreply.github.com>
2025-11-05 12:00:42 +0000

    feat: fetch allowed perms

--- src/api/role.ts
+++ src/api/role.ts
@@ -8,9 +8,9 @@ const role = new Hono().get(
 	"/all",
 	rbacCheck({ permissions: ["manage-users"] }),
 	async (c) => {
-		const { id, name } = getTableColumns(rolesTable);
+		const { id, name, allowed } = getTableColumns(rolesTable);
 
-		const roles = await db.select({ id, name }).from(rolesTable);
+		const roles = await db.select({ id, name, allowed }).from(rolesTable);
 
 		return c.json({ roles: roles });
 	},
