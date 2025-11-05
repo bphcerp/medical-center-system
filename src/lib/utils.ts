141d17820e5a586bceebeccb2752620662593348
Mohammed Zeeshan Valappil <66509181+zeeshanok@users.noreply.github.com>
2025-11-05 08:55:09 +0000

    refactor: move debounce to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export const debounce = <I, O>(callback: (input: I) => O, wait: number) => {
+  let timeoutId: NodeJS.Timeout;
+  return (input: I) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      callback(input);
+    }, wait);
+  };
+}
+
