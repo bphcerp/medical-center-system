38aab21bc8609b89c4e900565b5d9999c3ea89be
Mohammed Zeeshan Valappil <66509181+zeeshanok@users.noreply.github.com>
2025-11-10 17:45:21 +0000

    feat: add create, rename and delete role buttons

--- src/lib/types/permissions.ts
+++ src/lib/types/permissions.ts
@@ -5,6 +5,6 @@ export type Permission = (typeof permissions)[number];
 export const permissionDescriptions: Record<Permission, string> = {
   test: "Access to test features",
   vitals: "View and record patient vitals",
-  admin: "Create, edit, and delete user accounts",
+  admin: "Create, edit, and delete user accounts, roles and permissions",
   doctor: "Access to doctor-specific features",
 };
--- src/routes/admin/role.tsx
+++ src/routes/admin/role.tsx
@@ -1,10 +1,21 @@
 import { SelectTrigger } from "@radix-ui/react-select";
-import { createFileRoute, Link } from "@tanstack/react-router";
-import { ArrowLeftRight, Plus, X } from "lucide-react";
+import { createFileRoute, Link, useRouter } from "@tanstack/react-router";
+import { ArrowLeftRight, Pencil, Plus, Trash, X } from "lucide-react";
 import type React from "react";
 import { useState } from "react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
 import {
 	Select,
 	SelectContent,
@@ -28,7 +39,7 @@ import { cn, handleUnauthorized } from "@/lib/utils";
 import { client } from "../api/$";
 
 export const Route = createFileRoute("/admin/role")({
-	component: RouteComponent,
+	component: RolePage,
 	loader: async () => {
 		const data = await client.api.role.all.$get();
 		handleUnauthorized(data.status);
@@ -39,22 +50,64 @@ export const Route = createFileRoute("/admin/role")({
 	},
 });
 
-function RouteComponent() {
+function RolePage() {
 	const { roles } = Route.useLoaderData();
+	const router = useRouter();
 
-	const handleSave = async (id: number, perms: Permission[]) => {
+	const handleUpdateRole = async (
+		id: number,
+		name: string,
+		perms: Permission[],
+	) => {
 		const res = await client.api.role[":id"].$post({
 			param: { id: id.toString() },
-			json: { allowed: perms },
+			json: { name: name, allowed: perms },
 		});
 
-		return res.status === 200;
+		if (res.status !== 200) {
+			alert("Failed to update role");
+			return;
+		}
+
+		router.invalidate();
+	};
+
+	const handleDeleteRole = async (id: number) => {
+		const res = await client.api.role[":id"].$delete({
+			param: { id: id.toString() },
+		});
+
+		if (res.status !== 200) {
+			alert("Failed to delete role");
+			return;
+		}
+
+		router.invalidate();
+	};
+
+	const handleCreateRole = async (name: string) => {
+		const res = await client.api.role.$post({
+			json: {
+				name: name,
+				allowed: [],
+			},
+		});
+
+		if (res.status !== 200) {
+			alert("Failed to create role");
+			return;
+		}
+
+		router.invalidate();
 	};
 
 	return (
 		<div className="flex flex-col p-4 lg:p-10 lg:w-3/4 itms-start gap-3">
 			<div className="flex items-center justify-between">
-				<h1 className="text-2xl font-bold">Role Management</h1>
+				<span className="flex gap-4 items-center">
+					<h1 className="text-2xl font-bold inline">Role Management</h1>
+					<CreateRoleButton onCreate={handleCreateRole} />
+				</span>
 				<Link to="/admin/user">
 					<Button variant="link" className="p-0">
 						<ArrowLeftRight /> Manage users
@@ -64,8 +117,11 @@ function RouteComponent() {
 			<Table>
 				<TableHeader>
 					<TableRow>
+						<TableCell className="w-0 p-0" />
 						<TableCell className="w-1/4">Role</TableCell>
-						<TableCell>Permissions</TableCell>
+						<TableCell className="w-0 p-0" />
+						<TableCell className="pl-2">Permissions</TableCell>
+						<TableCell className="w-0 p-0" />
 					</TableRow>
 				</TableHeader>
 				<TableBody>
@@ -74,7 +130,10 @@ function RouteComponent() {
 							key={role.id}
 							name={role.name}
 							perms={role.allowed as Permission[]}
-							onSave={(perms) => handleSave(role.id, perms)}
+							onUpdateRole={(name, perms) =>
+								handleUpdateRole(role.id, name, perms)
+							}
+							onDeleteRole={() => handleDeleteRole(role.id)}
 						/>
 					))}
 				</TableBody>
@@ -83,99 +142,137 @@ function RouteComponent() {
 	);
 }
 
+function CreateRoleButton({
+	onCreate,
+}: {
+	onCreate: (name: string) => Promise<void>;
+}) {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleSubmit = async (e: FormData) => {
+		const name = e.get("roleName");
+		if (name === null) return;
+
+		await onCreate(name as string);
+		setIsOpen(false);
+	};
+
+	return (
+		<Dialog open={isOpen} onOpenChange={setIsOpen}>
+			<DialogTrigger asChild>
+				<Button variant="card" className="border-primary text-primary">
+					<Plus /> Create
+				</Button>
+			</DialogTrigger>
+			<DialogContent showCloseButton={false} className="gap-2">
+				<DialogTitle>Create new role</DialogTitle>
+				<DialogDescription>
+					You'll be able to assign permissions later.
+				</DialogDescription>
+				<form action={handleSubmit}>
+					<Input
+						type="text"
+						name="roleName"
+						placeholder="Role name"
+						className="mb-4"
+						autoFocus
+					/>
+					<DialogFooter>
+						<DialogClose asChild>
+							<Button variant="outline">Cancel</Button>
+						</DialogClose>
+
+						<Button type="submit">Create</Button>
+					</DialogFooter>
+				</form>
+			</DialogContent>
+		</Dialog>
+	);
+}
+
 function RowItem({
 	name,
-	onSave,
+	onDeleteRole,
+	onUpdateRole,
 	perms: originalPerms,
 }: {
 	name: string;
 	perms: Permission[];
-	onSave: (perms: Permission[]) => Promise<boolean>;
+	onUpdateRole: (name: string, perms: Permission[]) => Promise<void>;
+	onDeleteRole: () => Promise<void>;
 }) {
 	const [perms, setPerms] = useState(originalPerms);
 	const [unusedPerms, setUnusedPerms] = useState(
 		permissions.filter((p) => !originalPerms.includes(p)),
 	);
 
-	const [editMode, setEditMode] = useState(false);
-
 	const handleDeletePerm = (perm: Permission) => {
-		if (!editMode) setEditMode(true);
-		setPerms((prev) => prev.filter((p) => p !== perm));
-		setUnusedPerms((prev) => [...prev, perm]);
+		const newPerms = perms.filter((p) => p !== perm);
+		setPerms(newPerms);
+		// sort perms so they remain in the same place if and when re-adding
+		setUnusedPerms((prev) => [...prev, perm].sort());
+
+		onUpdateRole(name, newPerms);
 	};
 
 	const handleAddPerm = (perm: Permission) => {
-		if (!editMode) setEditMode(true);
-		setPerms((prev) => [...prev, perm]);
+		const newPerms = [perm, ...perms];
+		setPerms(newPerms);
 		setUnusedPerms((prev) => prev.filter((p) => p !== perm));
+
+		onUpdateRole(name, newPerms);
 	};
 
-	const resetPerms = () => {
-		setPerms(originalPerms);
-		setUnusedPerms(permissions.filter((p) => !originalPerms.includes(p)));
-	}
-
-	const handleCancelEdit = () => {
-		setEditMode(false);
-		resetPerms();
-	};
-
-	const handleSaveEdit = async () => {
-		const success = await onSave?.(perms);
-		if (!success) {
-			resetPerms();
-			alert("Failed to update permissions");
-		}
-		setEditMode(false);
+	const handleRename = async (newName: string) => {
+		await onUpdateRole(newName, perms);
 	};
 
 	return (
 		<TableRow>
+			<TableCell className="pr-0">
+				<RenameButton name={name} onRename={handleRename} />
+			</TableCell>
+
 			<TableCell>{name}</TableCell>
 
-			<TableCell
-				className={`flex transition-all gap-6 pr-2 ${editMode ? "py-4" : ""}`}
-			>
-				<div className="flex flex-1 flex-wrap transition-all gap-2 items-center">
-					<Select
-						onValueChange={(e) => handleAddPerm(e as Permission)}
-						disabled={unusedPerms.length === 0}
-					>
-						<SelectTrigger className="outline-none" asChild>
-							<PermBadge className="h-7 w-7 disabled:bg-transparent" asChild>
-								<Button
-									variant="card"
-									className="bg-primary/20 text-secondary"
-									size="sm"
-								>
-									<Plus />
-								</Button>
-							</PermBadge>
-						</SelectTrigger>
+			<TableCell className="flex gap-6 px-0">
+				<Select
+					onValueChange={(e) => handleAddPerm(e as Permission)}
+					defaultValue=""
+					disabled={unusedPerms.length === 0}
+				>
+					<SelectTrigger className="outline-none" asChild>
+						<CircleButton className="bg-primary/20 text-secondary">
+							<Plus />
+						</CircleButton>
+					</SelectTrigger>
 
-						<SelectContent>
-							<SelectGroup>
-								<SelectLabel>Permissions</SelectLabel>
+					<SelectContent align="start" className="max-w-80">
+						<SelectGroup>
+							<SelectLabel>Permissions</SelectLabel>
 
-								{unusedPerms.map((p) => (
-									<SelectItem value={p} key={p}>
-										<div className="flex flex-col">
-											<span className="font-medium">{p}</span>
-											<span className="text-muted-foreground text-xs">{permissionDescriptions[p]}</span>
-										</div>
-									</SelectItem>
-								))}
-							</SelectGroup>
-						</SelectContent>
-					</Select>
+							{unusedPerms.map((p) => (
+								<SelectItem value={p} key={p}>
+									<div className="flex flex-col">
+										<span className="font-medium">{p}</span>
+										<span className="text-muted-foreground text-xs">
+											{permissionDescriptions[p]}
+										</span>
+									</div>
+								</SelectItem>
+							))}
+						</SelectGroup>
+					</SelectContent>
+				</Select>
+			</TableCell>
 
+			<TableCell className="pl-2">
+				<div className="flex flex-1 flex-wrap gap-2 items-center">
 					{perms.map((perm) => (
 						<PermBadge
 							key={perm}
 							onClick={() => handleDeletePerm(perm)}
 							className="group"
-							destructive
 						>
 							<span className="translate-x-3 group-hover:translate-x-1 transition-transform">
 								{perm}
@@ -184,28 +281,112 @@ function RowItem({
 						</PermBadge>
 					))}
 				</div>
-				{editMode && (
-					<div className="ms-auto flex gap-2">
-						<Button variant="outline" size="sm" onClick={handleCancelEdit}>
-							Cancel
-						</Button>
-						<Button size="sm" onClick={handleSaveEdit}>
-							Save
-						</Button>
-					</div>
-				)}
+			</TableCell>
+			<TableCell>
+				<DeleteButton name={name} onDelete={onDeleteRole} />
 			</TableCell>
 		</TableRow>
 	);
 }
 
+function DeleteButton({
+	name,
+	onDelete,
+}: {
+	name: string;
+	onDelete: () => Promise<void>;
+}) {
+	const [isDeleteOpen, setIsDeleteOpen] = useState(false);
+
+	const handleDelete = async () => {
+		await onDelete();
+		setIsDeleteOpen(false);
+	};
+
+	return (
+		<Dialog open={isDeleteOpen} onOpenChange={setIsDeleteOpen}>
+			<DialogTrigger asChild>
+				<CircleButton className="hover:bg-destructive hover:text-destructive-foreground hover:border-destructive">
+					<Trash />
+				</CircleButton>
+			</DialogTrigger>
+
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Delete role "{name}"</DialogTitle>
+				</DialogHeader>
+
+				<DialogDescription>
+					Are you sure you want to delete this role? This action cannot be
+					undone.
+				</DialogDescription>
+
+				<DialogFooter>
+					<DialogClose asChild>
+						<Button variant="outline">Cancel</Button>
+					</DialogClose>
+					<Button variant="destructive" onClick={handleDelete}>
+						Delete
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
+}
+
+function RenameButton({
+	name,
+	onRename,
+}: {
+	name: string;
+	onRename: (name: string) => Promise<void>;
+}) {
+	const [isRenameOpen, setIsRenameOpen] = useState(false);
+
+	const handleRename = async (e: FormData) => {
+		const name = e.get("roleName");
+		if (name === null) return;
+
+		await onRename(name as string);
+		setIsRenameOpen(false);
+	};
+
+	return (
+		<Dialog open={isRenameOpen} onOpenChange={setIsRenameOpen}>
+			<DialogTrigger asChild>
+				<CircleButton>
+					<Pencil className="size-3.5" />
+				</CircleButton>
+			</DialogTrigger>
+			<DialogContent showCloseButton={false} className="gap-4">
+				<DialogTitle>Rename role "{name}"</DialogTitle>
+				<form action={handleRename}>
+					<Input
+						type="text"
+						name="roleName"
+						placeholder="Role name"
+						className="mb-4"
+						autoFocus
+					/>
+					<DialogFooter>
+						<DialogClose asChild>
+							<Button variant="outline">Cancel</Button>
+						</DialogClose>
+
+						<Button type="submit">Rename</Button>
+					</DialogFooter>
+				</form>
+			</DialogContent>
+		</Dialog>
+	);
+}
+
 function PermBadge({
 	onClick,
-	destructive = false,
 	className,
 	children,
 	...props
-}: React.PropsWithChildren<{ onClick?: () => void; destructive?: boolean }> &
+}: React.PropsWithChildren<{ onClick?: () => void }> &
 	React.ComponentProps<typeof Badge>) {
 	return (
 		<Badge
@@ -214,13 +395,29 @@ function PermBadge({
 			className={cn(
 				"font-medium py-1 px-1 text-sm/1 transition-colors flex gap-2 select-none",
 				"[&>svg]:size-4 hover:cursor-pointer",
-				destructive &&
-					"hover:bg-destructive hover:text-destructive-foreground hover:border-destructive",
+				"hover:bg-destructive hover:text-destructive-foreground hover:border-destructive",
 				className,
 			)}
 			{...props}
 		>
 			{children}
 		</Badge>
+	);
+}
+
+function CircleButton({
+	children,
+	className,
+	...props
+}: React.ComponentProps<typeof Button>) {
+	return (
+		<Button
+			variant="card"
+			className={cn("h-7 w-7 disabled:bg-transparent rounded-full", className)}
+			size="sm"
+			{...props}
+		>
+			{children}
+		</Button>
 	);
 }
