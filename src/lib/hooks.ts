c781c76fa430cd360e2e8b0df8fbdabb6bd409c2
Mohammed Zeeshan Valappil <66509181+zeeshanok@users.noreply.github.com>
2025-11-14 19:23:23 +0000

    feat: add window size hooks

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,9 +1,36 @@
-export const debounce = <I, O>(callback: (input: I) => O, wait: number) => {
-	let timeoutId: NodeJS.Timeout;
-	return (input: I) => {
-		clearTimeout(timeoutId);
-		timeoutId = setTimeout(() => {
-			callback(input);
-		}, wait);
+import { useEffect, useState } from "react";
+
+export function getWindowSize() {
+	return {
+		width: window.innerWidth,
+		height: window.innerHeight,
 	};
+}
+
+export function useWindowSize() {
+	const [windowSize, setWindowSize] = useState(getWindowSize());
+
+	useEffect(() => {
+		function handleResize() {
+			setWindowSize(getWindowSize());
+		}
+
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
+
+	return windowSize;
+}
+
+const breakpoints: Record<string, number> = {
+	sm: 640,
+	md: 768,
+	lg: 1024,
+	xl: 1280,
 };
+
+export function useWindowWidthAtLeast(breakpoint: "sm" | "md" | "lg" | "xl") {
+	const { width } = useWindowSize();
+
+	return width >= breakpoints[breakpoint];
+}
