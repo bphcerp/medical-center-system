733466a7c0364d1fe952a6dc498b366bce77acfb
Namit Bhutani <namit.bhut@gmail.com>
2025-11-16 15:13:33 +0530

    lint: formatting

--- src/routes/inventory.tsx
+++ src/routes/inventory.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/inventory')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/inventory")({
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/inventory"!</div>
+	return <div>Hello "/inventory"!</div>;
 }
