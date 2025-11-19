e6706d51f04d5f6663082907caa7037405f251ae
Namit Bhutani <namit.bhut@gmail.com>
2025-11-20 01:00:32 +0530

    fix: change db structure to work with new logic

--- src/db/otp.ts
+++ src/db/otp.ts
@@ -1,17 +1,27 @@
-import { integer, pgTable, timestamp } from "drizzle-orm/pg-core";
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { usersTable } from "./auth";
-import { patientsTable } from "./patient";
+import { casesTable } from "./case";
 
 export const doctorCaseHistoryOtpsTable = pgTable("doctor_case_history_otps", {
 	id: integer().primaryKey().generatedAlwaysAsIdentity(),
 	doctorId: integer()
-		.unique()
 		.references(() => usersTable.id)
 		.notNull(),
-	patientId: integer()
-		.unique()
-		.references(() => patientsTable.id)
+	caseId: integer()
+		.references(() => casesTable.id)
 		.notNull(),
 	otp: integer().notNull(),
+	createdAt: timestamp().notNull().defaultNow(),
+});
+
+export const otpOverrideLogsTable = pgTable("otp_override_logs", {
+	id: integer().primaryKey().generatedAlwaysAsIdentity(),
+	doctorId: integer()
+		.references(() => usersTable.id)
+		.notNull(),
+	caseId: integer()
+		.references(() => casesTable.id)
+		.notNull(),
+	reason: text().notNull(),
 	createdAt: timestamp().notNull().defaultNow(),
 });
