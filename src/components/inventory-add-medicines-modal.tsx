a4a5ff88a5143160833f6e5a22bfb724430af45f
knoxxus-dev <knoxxus.dev@gmail.com>
2025-11-20 08:56:37 +0000

    feat: add basic ui for adding new medicines modal

---
+++ src/components/inventory-add-medicines-modal.tsx
@@ -0,0 +1,257 @@
+// import { useRouter } from "@tanstack/react-router";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { ChevronsUpDown } from "lucide-react";
+import { useMemo, useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+	Command,
+	CommandEmpty,
+	CommandInput,
+	CommandItem,
+	CommandList,
+} from "@/components/ui/command";
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+} from "@/components/ui/dialog";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import type { Medicine } from "@/routes/inventory";
+
+export function AddMedicinesModal({
+	open,
+	onOpenChange,
+	medicines,
+}: {
+	open: boolean;
+	onOpenChange: (open: boolean) => void;
+	medicines: Medicine[];
+}) {
+	// const router = useRouter();
+
+	const [medicinesSearchOpen, setMedicinesSearchOpen] =
+		useState<boolean>(false);
+	const [medicineQuery, setMedicineQuery] = useState<string>("");
+
+	const filteredMedicines = useMemo(
+		() =>
+			medicines
+				.reduce(
+					(acc, medicine) => {
+						if (medicineQuery === "") {
+							acc.push({
+								medicine,
+								count: 0,
+							});
+							return acc;
+						}
+
+						const count = medicineQuery
+							.trim()
+							.split(/\s+/)
+							.reduce((count, term) => {
+								return (
+									count +
+									(medicine.drug.toLowerCase().includes(term.toLowerCase()) ||
+									medicine.brand.toLowerCase().includes(term.toLowerCase()) ||
+									medicine.company.toLowerCase().includes(term.toLowerCase()) ||
+									medicine.type.toLowerCase().includes(term.toLowerCase())
+										? 1
+										: 0)
+								);
+							}, 0);
+
+						if (count > 0) {
+							acc.push({
+								medicine,
+								count,
+							});
+						}
+						return acc;
+					},
+					[] as { medicine: (typeof medicines)[0]; count: number }[],
+				)
+				.sort((a, b) => b.count - a.count),
+		[medicines, medicineQuery],
+	);
+	const medicationListRef = useRef(null);
+	const medicationRowVirtualizer = useVirtualizer({
+		count: filteredMedicines.length,
+		getScrollElement: () => medicationListRef.current,
+		estimateSize: () => 48,
+		overscan: 15,
+		initialOffset: 0,
+	});
+
+	const [medicineItems, setMedicineItems] = useState<Medicine[]>([]);
+
+	const handleAddMedicineItem = (medicine: Medicine) => {
+		if (medicineItems.some((item) => item.id === medicine.id)) {
+			alert("This medicine is already in the list");
+			return;
+		}
+
+		setMedicineItems((prev) => [...prev, medicine]);
+		setMedicineQuery("");
+		setMedicinesSearchOpen(false);
+	};
+
+	const resetState = () => {
+		// setBatch("");
+		// setBatchError(false);
+		// setExpiry("");
+		// setExpiryError(false);
+		// setQuantity(0);
+		// setQuantityError(false);
+		// setApiError(false);
+	};
+
+	const handleSubmit = async () => {
+		// if (quantity == null || quantity <= 0) {
+		// 	resetState();
+		// 	setQuantityError(true);
+		// 	return;
+		// }
+		// if (batch == null) {
+		// 	resetState();
+		// 	setBatchError(true);
+		// 	return;
+		// }
+		// if (expiry === "") {
+		// 	resetState();
+		// 	setExpiryError(true);
+		// 	return;
+		// }
+		// const res = await client.api.inventory.batch.$post({
+		// 	json: { medicineId: medicine.id, batchNum: batch, expiry, quantity },
+		// });
+		// if (res.status === 200) {
+		// 	await router.invalidate();
+		// 	resetState();
+		// 	onOpenChange(false);
+		// } else {
+		// 	resetState();
+		// 	setApiError(true);
+		// }
+	};
+
+	const handleCancel = () => {
+		resetState();
+		onOpenChange(false);
+	};
+
+	if (!medicines) return null;
+
+	return (
+		<Dialog open={open} onOpenChange={onOpenChange}>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Add New Medicines</DialogTitle>
+					<DialogDescription>
+						Add new medicines to the inventory
+					</DialogDescription>
+				</DialogHeader>
+				<Popover
+					open={medicinesSearchOpen}
+					onOpenChange={setMedicinesSearchOpen}
+				>
+					<PopoverTrigger asChild>
+						<Button
+							variant="outline"
+							role="combobox"
+							className="justify-between"
+						>
+							Select a medicine...
+							<ChevronsUpDown className="ml-2 h-4 w-4" />
+						</Button>
+					</PopoverTrigger>
+					<PopoverContent className="p-0 w-3xl" align="start" side="top">
+						<Command shouldFilter={false}>
+							<CommandInput
+								placeholder="Type a medicine to search..."
+								value={medicineQuery}
+								onValueChange={setMedicineQuery}
+							/>
+							<CommandList ref={medicationListRef}>
+								<div
+									style={{
+										height:
+											medicationRowVirtualizer.getTotalSize() > 0
+												? `${medicationRowVirtualizer.getTotalSize()}px`
+												: "auto",
+									}}
+									className="relative w-full"
+								>
+									<CommandEmpty>No medicines found.</CommandEmpty>
+									{medicationRowVirtualizer
+										.getVirtualItems()
+										.map((virtualItem) => {
+											const medicine =
+												filteredMedicines[virtualItem.index].medicine;
+											return (
+												<CommandItem
+													key={virtualItem.key}
+													onSelect={() => {
+														handleAddMedicineItem(medicine);
+														setMedicinesSearchOpen(false);
+													}}
+													className="flex absolute top-0 left-0 w-full justify-between"
+													style={{
+														height: `${virtualItem.size}px`,
+														transform: `translateY(${virtualItem.start}px)`,
+													}}
+												>
+													<span>
+														{medicine.company} {medicine.brand}
+													</span>
+													<span className="mx-1 text-muted-foreground text-right">
+														({medicine.drug}) - {medicine.strength} -{" "}
+														{medicine.type}
+													</span>
+												</CommandItem>
+											);
+										})}
+								</div>
+							</CommandList>
+						</Command>
+					</PopoverContent>
+				</Popover>
+				{medicineItems.length > 0 ? (
+					medicineItems.map((item) => (
+						<div key={item.id} className="px-2">
+							<div className="w-full pb-1 flex flex-wrap items-center gap-2">
+								<span className="font-semibold">
+									{item.company} {item.brand}
+								</span>
+								<span className="text-muted-foreground text-sm">
+									({item.drug}) - {item.strength}
+								</span>
+								<span className="text-xs px-2 py-0.5 rounded-full bg-primary/10 text-primary">
+									{item.type}
+								</span>
+							</div>
+						</div>
+					))
+				) : (
+					<p>No medicines have been added</p>
+				)}
+				<div>
+					<Button
+						className="my-2 mr-2"
+						onClick={handleCancel}
+						variant="outline"
+					>
+						Cancel
+					</Button>
+					<Button onClick={handleSubmit}>Submit</Button>
+				</div>
+			</DialogContent>
+		</Dialog>
+	);
+}
