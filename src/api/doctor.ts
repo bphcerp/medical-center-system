a159f8393938f19eaf83beac33830b77f44f922e
soumitradev <soumitra.shewale.dev@gmail.com>
2025-11-21 07:45:20 +0000

    feat: implement lab test search on consultation lab request modal

--- src/api/doctor.ts
+++ src/api/doctor.ts
@@ -380,7 +380,6 @@ const doctor = new Hono()
 			.select({
 				id: labTestsMasterTable.id,
 				name: labTestsMasterTable.name,
-				description: labTestsMasterTable.description,
 				category: labTestsMasterTable.category,
 			})
 			.from(labTestsMasterTable)
