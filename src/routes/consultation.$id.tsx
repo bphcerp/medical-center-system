dbd047900ed2bbcc216df55d99f0a36b99b027fb
soumitradev <soumitra.shewale.dev@gmail.com>
2025-11-27 10:54:18 +0000

    feat: use PatientDetails component on consultation page

--- src/routes/consultation.$id.tsx
+++ src/routes/consultation.$id.tsx
@@ -8,12 +8,12 @@ import FinalizeCaseCard, {
 	type FinalizeButtonValue,
 } from "@/components/finalize-case-card";
 import LabRequestModal from "@/components/lab-request-modal";
+import { PatientDetails } from "@/components/patient-details";
 import PrescriptionCard from "@/components/prescription/prescription-card";
 import TopBar from "@/components/topbar";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Textarea } from "@/components/ui/textarea";
-import VitalField from "@/components/vital-field";
 import VitalsCard from "@/components/vitals-card";
 import useAuth from "@/lib/hooks/useAuth";
 import { useAutosave } from "@/lib/hooks/useAutosave";
@@ -163,32 +163,31 @@ function ConsultationPage() {
 	return (
 		<>
 			<TopBar title="Consultation Page" />
-			<div className="p-6">
+			<div className="px-6 py-4">
 				<div className="flex justify-between items-start mb-4">
-					<div>
-						<h1 className="text-3xl font-bold">
-							Consultation for {caseDetail.patient.name}
-						</h1>
-						<div className="flex gap-4 items-center text-muted-foreground ">
-							<p className="my-2">Token Number: {caseDetail.cases.token}</p>
-							<span
-								className={`my-2 flex items-center gap-2 ${autosaveError ? "text-destructive" : ""}`}
-							>
-								{autosaveError ? (
-									<>
-										<TriangleAlert className="size-4" />
-										{autosaveError}
-									</>
-								) : autosaved ? (
-									<CloudCheck className="size-4" />
-								) : (
-									<>
-										<RefreshCw className="animate-spin size-4" />
-										Saving...
-									</>
-								)}
-							</span>
-						</div>
+					<div className="flex gap-4 items-end">
+						<PatientDetails
+							patient={caseDetail.patient}
+							token={caseDetail.cases.token}
+							label="Consultation for"
+						/>
+						<span
+							className={`py-2 flex items-center gap-2 ${autosaveError ? "text-destructive" : "text-muted-foreground"}`}
+						>
+							{autosaveError ? (
+								<>
+									<TriangleAlert className="size-4" />
+									{autosaveError}
+								</>
+							) : autosaved ? (
+								<CloudCheck className="size-4" />
+							) : (
+								<>
+									<RefreshCw className="animate-spin size-4" />
+									Saving...
+								</>
+							)}
+						</span>
 					</div>
 					<div className="flex gap-2">
 						<Button
@@ -219,24 +218,6 @@ function ConsultationPage() {
 					setLabTestModalOpen={setLabTestModalOpen}
 					tests={tests}
 				/>
-				{/* TODO: Standardize this vitals layout and make it a component also used in the vitals page */}
-				<Card className="mb-2">
-					<div className="flex gap-4 mx-3">
-						<VitalField
-							label="Patient Name"
-							type="text"
-							value={caseDetail?.patient.name}
-							readonly
-						/>
-						<VitalField label="Age" value={caseDetail?.patient.age} readonly />
-						<VitalField
-							label="ID/PSRN/Phone"
-							type="text"
-							value={caseDetail?.identifier}
-							readonly
-						/>
-					</div>
-				</Card>
 				<VitalsCard vitals={caseDetail.cases} />
 				<div className="grid grid-cols-3 mb-2">
 					<Card className="col-span-1 row-span-2 rounded-r-none rounded-bl-none px-2 pt-4 pb-2">
